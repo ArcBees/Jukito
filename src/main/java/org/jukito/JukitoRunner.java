8f4308c94f78413a77bffecc29f89d661cb2089d
Philippe Beaudoin <philippe.beaudoin@gmail.com>
2011-05-27 10:51:45 -0400

    Minor corrections.

--- src/main/java/org/jukito/JukitoRunner.java
+++ src/main/java/org/jukito/JukitoRunner.java
@@ -288,10 +288,9 @@ public class JukitoRunner extends BlockJUnit4ClassRunner {
 
   /**
    * Adds to {@code errors} if any method in this class is annotated with
-   * {@code annotation}, but:
+   * the provided {@code annotation}, but:
    * <ul>
    * <li>is not public, or
-   * <li>takes parameters, or
    * <li>returns something other than void, or
    * <li>is static (given {@code isStatic is false}), or
    * <li>is not static (given {@code isStatic is true}).
@@ -302,7 +301,6 @@ public class JukitoRunner extends BlockJUnit4ClassRunner {
 
     for (FrameworkMethod eachTestMethod : methods) {
       eachTestMethod.validatePublicVoid(isStatic, errors);
-      // TODO We should validate that all parameters have a binding
     }
   }
 
