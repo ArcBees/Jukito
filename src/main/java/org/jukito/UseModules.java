2d6743346eac383aaec38d09a559a2c771026e92
Julian Lettner <julian.lettner@gmail.com>
2013-03-16 12:42:11 +0100

    Add docs to @UseModules.

--- src/main/java/org/jukito/UseModules.java
+++ src/main/java/org/jukito/UseModules.java
@@ -7,6 +7,36 @@ import java.lang.annotation.Target;
 
 import com.google.inject.Module;
 
+/**
+ * This annotation can be used on a test class together with
+ * {@code @RunWith(JukitoRunner.class)} to use the bindings contained
+ * in the specified modules for the test.
+ * <p/>
+ * Example:
+ * <pre>
+ * {@literal @}RunWith(JukitoRunner.class)
+ * {@literal @}UseModules({ FooModule.class, BarModule.class}
+ * public class MyTest {
+ *   // Tests methods  
+ * }</pre>
+ * 
+ * The example is equivalent to the following <i>inner static module
+ * class</i> approach.
+ * <pre>
+ * {@literal @}RunWith(JukitoRunner.class)
+ * public class MyTest {
+ *   static class Module extends JukitoModule {
+ *     {@literal @}Override
+ *     protected void configureTest() {
+ *       install(new FooModule());
+ *       install(new BarModule());
+ *     }
+ *   }
+ *   // Test methods
+ * }</pre>
+ * 
+ * @author Julian Lettner
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface UseModules {
