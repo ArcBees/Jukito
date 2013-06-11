d3ee88fbc5c1f8bda3935bccb466033c138388ab
przemekgalazka <przemek.galazka@gmail.com>
2013-06-11 10:39:44 +0200

    checkstyle

--- jukito-samples/src/test/java/org/jukito/samples/FordMustangTest3.java
+++ jukito-samples/src/test/java/org/jukito/samples/FordMustangTest3.java
@@ -1,6 +1,20 @@
-package org.jukito.samples;
+/**
+ * Copyright 2011 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 
-import javax.inject.Inject;
+package org.jukito.samples;
 
 import org.jukito.JukitoModule;
 import org.jukito.JukitoRunner;
@@ -8,11 +22,12 @@ import org.jukito.samples.modules.DieselLineModule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import javax.inject.Inject;
+
 import static org.mockito.Mockito.verify;
 
-
 /**
- * A simple test with one real DOC ( binding via module installation
+ * A simple test with one real DOC ( binding via module installation.
  *
  * @author Przemysław Gałązka
  * @since 10-06-2013
@@ -34,6 +49,9 @@ public class FordMustangTest3 {
         verify(sut.getEngine()).initiateIgnition();
     }
 
+    /**
+     * JukitoModule.
+     */
     public static class A extends JukitoModule {
         @Override
         protected void configureTest() {
